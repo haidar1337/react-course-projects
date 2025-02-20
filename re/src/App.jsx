ba1c8f5d7f7cbac59d5b4c9fc99eab8b3d8dc470
haidar <haidar.abugurain@gmail.com>
2025-02-20 18:22:44 +0300

    components, props, file structure

--- re/src/App.jsx
+++ re/src/App.jsx
@@ -1,21 +1,32 @@
-function Header() {
-  return (
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
+import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header.jsx';
+import Concept from './components/Concept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
   return (
     <div>
+      <Header/>
       <main>
+        <section id='core-concepts'>
         <h2>Time to get started!</h2>
+        <ul>
+          <Concept title="Components" description="The core UI building block - compose the user interface by combining multiple components." image={componentsImg}/>
+          <Concept {...CORE_CONCEPTS[1]}/>
+          <Concept {...CORE_CONCEPTS[2]}/>
+          <Concept {...CORE_CONCEPTS[3]}/>
+        </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
---
+++ re/src/components/Concept.jsx
@@ -0,0 +1,12 @@
+/* 
+  An alternative syntax is to destructure the passed object CoreConcept({image, title, description})
+*/
+export default function Concept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}/>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  )
+}
