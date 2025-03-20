cb85fc6472de11eea7c6ef11f206fd316243ecfc
haidar <haidar.abugurain@gmail.com>
2025-03-20 19:11:34 +0300

    finish section

--- styrc/src/components/Header/Header.jsx
+++ styrc/src/components/Header/Header.jsx
@@ -1,14 +1,13 @@
 import logo from "../../assets/logo.png";
-import classes from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p className={classes.paragraph}>
-        A community of artists and art-lovers.
-      </p>
+    <header className="flex flex-col items-center mt-8 md:mb-16">
+      <img src={logo} alt="A canvas" className="mb-8 w-44 h-44" />
+      <h1 className="text-amber-800 font-semibold text-xl md:text-4xl tracking-widest text-center uppercase font-title">
+        ReactArt
+      </h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
